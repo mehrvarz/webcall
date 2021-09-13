dbd61ab15cc8d35527ef2cbc821b50b8d8515b4f
timur.mehrvarz <timur.mehrvarz@riseup.net>
2021-09-13 16:08:29 +0200

    added adapter-latest.js to init.sh; removed webroot/adapter-latest.js

--- init.sh
+++ init.sh
@@ -6,4 +6,6 @@ echo 'const gentle = true;' >> webroot/callee/custom.js
 echo 'const apiPath = "/rtcsig";' > webroot/button/custom.js
 echo 'const gentle = true;' >> webroot/button/custom.js
 echo 'const singlebutton = true;' >> webroot/button/custom.js
+wget https://github.com/webrtcHacks/adapter/raw/master/release/adapter.js
+mv adapter.js webroot/adapter-latest.js
 
