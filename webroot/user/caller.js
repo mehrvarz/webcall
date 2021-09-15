c55e06843a2f1427f410cd5cc859727bc6b0c79e
timur.mehrvarz <timur.mehrvarz@riseup.net>
2021-09-15 18:37:25 +0200

    caller.js: test: sending hardcoded server-ip instead of window.location.hostname

--- webroot/user/caller.js
+++ webroot/user/caller.js
@@ -1389,8 +1389,10 @@ function dial() {
 
 	var ICE_config= {
 		"iceServers": [
-			{	'urls': 'stun:'+window.location.hostname+':3739' },
-			{	'urls': 'turn:'+window.location.hostname+':3739',
+//			{	'urls': 'stun:'+window.location.hostname+':3739' },
+//			{	'urls': 'turn:'+window.location.hostname+':3739',
+			{	'urls': 'stun:66.228.46.43:3739' },
+			{	'urls': 'turn:66.228.46.43:3739',
 				'username': 'c807ec29df3c9ff',
 				'credential': '736518fb4232d44'
 			}
