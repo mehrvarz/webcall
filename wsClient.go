73c474ed9a68629ec0d7b498aaf680b091f00b80
timur.mehrvarz <timur.mehrvarz@riseup.net>
2022-01-17 02:09:51 +0100

    wsClient.go log 'serveWss peer' without rip, but with (callerName:callerID)

--- wsClient.go
+++ wsClient.go
@@ -617,8 +617,8 @@ func (c *WsClient) receiveProcess(message []byte) {
 	}
 
 	if cmd=="log" {
-		fmt.Printf("%s peer %s %s rip=%s (%s:%s)\n", c.connType, payload, c.calleeID, c.RemoteAddr,
-			c.hub.CallerClient.callerID, c.hub.CallerClient.callerName)
+		fmt.Printf("%s peer %s %s (%s:%s)\n", c.connType, payload, c.calleeID,
+			c.hub.CallerClient.callerName, c.hub.CallerClient.callerID)
 		tok := strings.Split(payload, " ")
 		if len(tok)>=3 {
 			// callee Connected p2p/p2p port=10001 id=3949620073
