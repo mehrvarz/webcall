664a2f72467ca1b7758f5b2b80b6616b9682de7d
timur.mehrvarz <timur@timur.mobi>
2022-02-04 03:42:29 +0100

    init.sh now installs adapter-latest.js v8.1.1

--- init.sh
+++ init.sh
@@ -6,7 +6,8 @@ echo 'const gentle = true;' >> webroot/callee/custom.js
 echo 'const apiPath = "/rtcsig";' > webroot/button/custom.js
 echo 'const gentle = true;' >> webroot/button/custom.js
 echo 'const singlebutton = true;' >> webroot/button/custom.js
-wget https://webrtc.github.io/adapter/adapter-8.1.0.js
-cp adapter-8.1.0.js webroot/adapter-latest.js
-rm adapter-8.1.0.js
+#wget https://webrtc.github.io/adapter/adapter-8.1.0.js
+wget https://github.com/webrtcHacks/adapter/raw/gh-pages/adapter-8.1.1.js
+cp adapter-8.1.1.js webroot/adapter-latest.js
+rm adapter-8.1.1.js
 
