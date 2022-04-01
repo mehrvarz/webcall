7768e23adc11dfe75a78e607ab9804686357f057
timur.mehrvarz <timur@timur.mobi>
2022-04-01 23:28:13 +0200

    2.0.10 httpLogin.go: denied login after ws-connect timeout: 'fatal Websocket communication problem detected. Please check your... Account blocked for 20 minutes.' (do not delete(blockMap,urlID))

--- httpLogin.go
+++ httpLogin.go
@@ -43,7 +43,7 @@ func httpLogin(w http.ResponseWriter, r *http.Request, urlID string, cookie *htt
 		//delete(blockMap,urlID)
 		//blockMapMutex.Unlock()
 		if time.Now().Sub(blockedTime) < 20 * time.Minute {
-			fmt.Fprintf(w,"fatal Websocket communication problem detected. Please check your System WebView and network settings")
+			fmt.Fprintf(w,"fatal Websocket communication problem detected. Please check your System WebView and network settings. Account blocked for 20 minutes.")
 			fmt.Printf("/login (%s) blocked rip=%s ua=%s ver=%s\n",
 				urlID, remoteAddr, userAgent, clientVersion)
 			return
