869c046a160f423c7e1c4d97e412f045d640094d
timur.mehrvarz <timur@timur.mobi>
2022-04-06 14:33:44 +0200

    httpOnline.go /online to log (callerID:callerName)

--- httpOnline.go
+++ httpOnline.go
@@ -37,6 +37,18 @@ func httpOnline(w http.ResponseWriter, r *http.Request, urlID string, remoteAddr
 		clientVersion = url_arg_array[0]
 	}
 
+	callerId := ""
+	url_arg_array, ok = r.URL.Query()["callerId"]
+	if ok && len(url_arg_array[0]) >= 1 {
+		callerId = url_arg_array[0]
+	}
+
+	callerName := ""
+	url_arg_array, ok = r.URL.Query()["callerName"]
+	if ok && len(url_arg_array[0]) >= 1 {
+		callerName = url_arg_array[0]
+	}
+
 	// we look for urlID either in the local or in the global hubmap
 	reportHiddenCallee := true
 	reportBusyCallee := true
@@ -181,13 +193,15 @@ func httpOnline(w http.ResponseWriter, r *http.Request, urlID string, remoteAddr
 		}
 		wsAddr = fmt.Sprintf("%s?wsid=%d", wsAddr, wsClientID)
 
-		fmt.Printf("/online (%s) avail wsAddr=%s rip=%s\n", glUrlID, wsAddr, remoteAddr)
+		fmt.Printf("/online (%s) avail wsAddr=%s (%s:%s) rip=%s\n",
+			glUrlID, wsAddr, callerId, callerName, remoteAddr)
 		fmt.Fprintf(w, wsAddr)
 		return
 	}
 
 	// something has gone wrong - callee not found anywhere
-	fmt.Printf("# /online (%s/%s) no hub found for rip=%s\n", urlID, glUrlID, remoteAddr)
+	fmt.Printf("# /online (%s/%s) not found (%s:%s) rip=%s\n",
+		urlID, glUrlID, callerId, callerName, remoteAddr)
 
 	// clear ConnectedCallerIp
 	StoreCallerIpInHubMap(glUrlID, "", false)
