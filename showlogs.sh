2f49000f060ff6877303d587d7f901fc62ade6b7
timurmobi <timur.mehrvarz@gmail.com>
2022-04-07 14:48:53 +0000

    showlogs.sh now using 'cut' to filter out two columns ('timur webcall[23501]:')

--- showlogs.sh
+++ showlogs.sh
@@ -1,4 +1,5 @@
 # shows the logs when WebCall server is running as systemd service
+# tail -f /var/log/syslog |grep --line-buffered " webcall" |grep --line-buffered -vE "csp-report|_DeleteTweet|17850068781|68713529299|38026732534|no pw" |cut -d ' ' --complement -f5-6
 tail -8000 /var/log/syslog |grep --line-buffered " webcall" |cut -d ' ' --complement -f5-6
 tail -f /var/log/syslog |grep --line-buffered " webcall"|cut -d ' ' --complement -f5-6
 
