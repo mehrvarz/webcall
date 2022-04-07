9c6d34c9c245004999ad95976f4b12f3846adbc2
timurmobi <timur.mehrvarz@gmail.com>
2022-04-07 14:45:53 +0000

    showlogs.sh now using 'cut' to filter out two columns ('timur webcall[23501]:')

--- showlogs.sh
+++ showlogs.sh
@@ -1,4 +1,4 @@
 # shows the logs when WebCall server is running as systemd service
-tail -8000 /var/log/syslog |grep " webcall" #|grep -vE "No Permission or Channel"
-tail -f /var/log/syslog |grep " webcall" #|grep -vE "No Permission or Channel"
+tail -8000 /var/log/syslog |grep --line-buffered " webcall" |cut -d ' ' --complement -f5-6
+tail -f /var/log/syslog |grep --line-buffered " webcall"|cut -d ' ' --complement -f5-6
 
