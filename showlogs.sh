31fb18465879472fc3d6a3bdddd5e51b574b2cdf
timurmobi <timur.mehrvarz@gmail.com>
2022-04-11 23:48:21 +0000

    removed showlogs.sh

--- showlogs.sh
+++
@@ -1,5 +0,0 @@
-# shows the logs when WebCall server is running as systemd service
-# tail -f /var/log/syslog |grep --line-buffered " webcall" |grep --line-buffered -vE "csp-report|_DeleteTweet" |cut -d ' ' --complement -f5-6
-tail -8000 /var/log/syslog |grep --line-buffered " webcall" |cut -d ' ' --complement -f5-6
-tail -f /var/log/syslog |grep --line-buffered " webcall"|cut -d ' ' --complement -f5-6
-
