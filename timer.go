3aa4078820f96a7f46a032c2e723d082d6edcbf4
timur.mehrvarz <timur@timur.mobi>
2022-04-15 01:54:52 +0200

    timer.go ticker20min(): list of followerIDs behind if logWantedFor('twitter')

--- timer.go
+++ timer.go
@@ -140,7 +140,7 @@ func ticker20min() {
 			fmt.Printf("# ticker20min no twitterClient\n")
 		} else {
 			// download list of followers
-			fmt.Printf("ticker20min download list of all followers...\n")
+			fmt.Printf("ticker20min download list of twitter followers...\n")
 			// TODO we must later support more than 5000 followers
 			var err error
 			followerIDsLock.Lock()
