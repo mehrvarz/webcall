80745d9d5ab3e04279124e255f7620570512915f
timur.mehrvarz <timur@timur.mobi>
2022-05-05 12:22:27 +0200

    httpOnline.go: 'notavailtemp' if secsSinceLogoff < 3*60 (was 9*60)

--- httpOnline.go
+++ httpOnline.go
@@ -102,7 +102,7 @@ func httpOnline(w http.ResponseWriter, r *http.Request, urlID string, remoteAddr
 			// use dbUser.LastLogoffTime to see how long it has been offline
 			secsSinceLogoff = time.Now().Unix() - dbUser.LastLogoffTime
 		}
-		if secsSinceLogoff>0 && secsSinceLogoff < 9*60 {
+		if secsSinceLogoff>0 && secsSinceLogoff < 3*60 {
 			// callee may come back very soon
 			fmt.Printf("/online (%s) is offline temp (for %d secs) %s ver=%s ua=%s\n",
 				urlID, secsSinceLogoff, remoteAddr, clientVersion, r.UserAgent())
