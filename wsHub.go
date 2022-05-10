6621485e09ec13384549fec5c1302d04fadc390f
timur.mehrvarz <timur@timur.mobi>
2022-05-10 19:06:29 +0200

    wsHub.go removed old outremarked code

--- wsHub.go
+++ wsHub.go
@@ -84,7 +84,6 @@ func (h *Hub) setDeadline(secs int, comment string) {
 					if h.CalleeClient.isConnectedToPeer.Get() {
 						fmt.Printf("setDeadline (%s) reached; end session now (secs=%d %v)\n",
 							h.CalleeClient.calleeID, secs, timeStart.Format("2006-01-02 15:04:05"))
-						//h.doUnregister(h.CalleeClient,"setDeadline "+comment)
 						h.doBroadcast([]byte("cancel|c"))
 						h.CalleeClient.peerConHasEnded("deadline")
 						if(h.CallerClient!=nil) {
