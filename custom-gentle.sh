f590e70a4cb99481d953dd22d2ea7638ddbe57e4
timur.mehrvarz <timur@timur.mobi>
2022-06-01 19:39:54 +0200

    added custom-gentle.sh

---
+++ custom-gentle.sh
@@ -0,0 +1,8 @@
+sed -i 's/gentle = false/gentle = true/' webroot/user/custom.js
+sed -i 's/gentle = false/gentle = true/' webroot/callee/settings/custom.js
+sed -i 's/gentle = false/gentle = true/' webroot/callee/contacts/custom.js
+sed -i 's/gentle = false/gentle = true/' webroot/callee/register/custom.js
+sed -i 's/gentle = false/gentle = true/' webroot/callee/custom.js
+sed -i 's/gentle = false/gentle = true/' webroot/button/custom.js
+
+
