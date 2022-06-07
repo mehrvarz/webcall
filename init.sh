3907f3bb9ca2167eed366ab51f4d444fc6b5818c
timur.mehrvarz <timur@timur.mobi>
2022-06-07 13:41:55 +0200

    init.sh creating hard links for user/client.js and button/client.js

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
 wget https://github.com/webrtcHacks/adapter/raw/gh-pages/adapter-8.1.1.js
 cp adapter-8.1.1.js webroot/callee/adapter-latest.js
 rm adapter-8.1.1.js
-rm webroot/user/adapter-latest.js
+rm -f webroot/user/adapter-latest.js
 ln webroot/callee/adapter-latest.js webroot/user/adapter-latest.js
-rm webroot/button/adapter-latest.js
+rm -f webroot/button/adapter-latest.js
 ln webroot/callee/adapter-latest.js webroot/button/adapter-latest.js
+rm -f webroot/user/client.js
+ln webroot/callee/client.js webroot/user/client.js
+rm -f webroot/button/client.js
+ln webroot/callee/client.js webroot/button/client.js
+
+
 
