fc39dc9c3f58e5aad16b63f754b3907211bd491b
timur.mehrvarz <timur@timur.mobi>
2022-06-30 06:23:04 +0200

    deactivated initwebr.sh

--- initwebr.sh
+++ initwebr.sh
@@ -1,12 +1,12 @@
-rm -f webroot/user/client.js
-ln webroot/callee/client.js webroot/user/client.js
-rm -f webroot/button/client.js
-ln webroot/callee/client.js webroot/button/client.js
-rm -f webroot/button/caller.js
-ln webroot/user/caller.js webroot/button/caller.js
+#rm -f webroot/user/client.js
+#ln webroot/callee/client.js webroot/user/client.js
+#rm -f webroot/button/client.js
+#ln webroot/callee/client.js webroot/button/client.js
+#rm -f webroot/button/caller.js
+#ln webroot/user/caller.js webroot/button/caller.js
 
-rm -f webroot/user/prefercodec.js
-ln webroot/callee/prefercodec.js webroot/user/prefercodec.js
-rm -f webroot/button/prefercodec.js
-ln webroot/callee/prefercodec.js webroot/button/prefercodec.js
+#rm -f webroot/user/prefercodec.js
+#ln webroot/callee/prefercodec.js webroot/user/prefercodec.js
+#rm -f webroot/button/prefercodec.js
+#ln webroot/callee/prefercodec.js webroot/button/prefercodec.js
 
