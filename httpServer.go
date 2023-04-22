de034dcf0b5a8bd0e8c73de949b466aa0a950a3a
timur.mehrvarz <timur@timur.mobi>
2023-04-22 16:18:13 +0200

    httpServer.go: /dumponline 22 -> 25 chars for calleeID

--- httpServer.go
+++ httpServer.go
@@ -867,7 +867,7 @@ func httpApiHandler(w http.ResponseWriter, r *http.Request) {
 				}
 
 				calleeID := hubSlice[idx].CalleeClient.calleeID // or globalCalleeID
-				boldString, _ := strconv.Unquote(`"\033[1m` + fmt.Sprintf("%-22s",calleeID) + `\033[0m"`)
+				boldString, _ := strconv.Unquote(`"\033[1m` + fmt.Sprintf("%-25s",calleeID) + `\033[0m"`)
 				fmt.Fprintf(w,"%s %-15s %-21s %s %s\n",
 					boldString,
 					hubSlice[idx].CalleeClient.RemoteAddrNoPort,
