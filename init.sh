d8c90ba263ea7e1e53c81c00edfa473cded5493a
timur.mehrvarz <timur@timur.mobi>
2023-05-16 13:58:45 +0200

    init.sh updated: fetch 8.2.2 instead of 8.1.1; removed symlink for buttons

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
-wget https://github.com/webrtcHacks/adapter/raw/gh-pages/adapter-8.1.1.js
-cp adapter-8.1.1.js webroot/callee/adapter-latest.js
-rm adapter-8.1.1.js
+rm adapter-8.2.2.js
+wget https://github.com/webrtcHacks/adapter/raw/gh-pages/adapter-8.2.2.js
+cp adapter-8.2.2.js webroot/callee/adapter-latest.js
+rm adapter-8.2.2.js
 rm -f webroot/user/adapter-latest.js
 ln webroot/callee/adapter-latest.js webroot/user/adapter-latest.js
-rm -f webroot/button/adapter-latest.js
-ln webroot/callee/adapter-latest.js webroot/button/adapter-latest.js
 
