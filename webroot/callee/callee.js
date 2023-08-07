6959c102c0548b799d33261c0a4b7e5e7920f65e
timur.mehrvarz <timur@timur.mobi>
2023-08-07 03:30:03 +0200

    callee.js on Android: 'Your Webcall ID's (long-tap to copy/share)'

--- webroot/callee/callee.js
+++ webroot/callee/callee.js
@@ -895,9 +895,9 @@ function getSettingDone() {
 		let links = "";
 		links += "<div style='line-height:1.6em;margin-top:12px;white-space:nowrap;'>";
 		if(typeof Android !== "undefined" && Android !== null) {
-			links += "<div class='callListTitle'>Your Webcall ID's (long-tap to copy/share link):</div>";
+			links += "<div class='callListTitle'>Your Webcall ID's (long-tap to copy/share):</div>";
 		} else {
-			links += "<div class='callListTitle'>Your Webcall ID's (right-click to copy link):</div>";
+			links += "<div class='callListTitle'>Your Webcall ID's (right-click to copy):</div>";
 		}
 
 		if(mainLinkDeactive) {
